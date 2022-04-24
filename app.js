e8235d4b614d67a7ac73cf71fae31d157ab6b24d
Lavesh <79662096+lavesh01@users.noreply.github.com>
2022-04-24 22:10:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -151,5 +151,5 @@ if (port == null || port == "") {
 }
 
 app.listen(port, function () {
-  console.log("Server has started successfully!");
+  console.log("Server has started successfully on horeku!");
 });
